7c6f6e1cd5f00157dcd444a8b4d112a11d6413e9
Aysr01 <sarab.ayoub@inemail.ine.inpt.ma>
2024-03-22 16:22:20 +0000

    handled some exceptions

--- dags/stats_dag/utils/statistics_scraper.py
+++ dags/stats_dag/utils/statistics_scraper.py
@@ -18,7 +18,7 @@ class StatsScraper():
         with open(os.environ.get("PROXIES_PATH"), "r") as f: 
             self.proxies = f.read().split("\n")
 
-    def get_match_data(self, match_id):
+    def get_match_stats(self, match_id):
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
         }
@@ -38,41 +38,48 @@ class StatsScraper():
                 response = None 
         return response
     
-    def extract_stats(self, stats_data):
+    def extract_desired_stats(self, stats_data):
         stats = {}
-        try:
-            for period_data in stats_data:
-                # period_data contains stats of first half, second half, or the entire match 
+        if not stats_data:
+            logger.error("No data to extract statistics from")
+            return {}
+        for period_data in stats_data:
+            # period_data contains stats of first half, second half, or the entire match
+            try:
                 groups = period_data["groups"]
+            except KeyError as e:
+                logger.error(f"KeyError: {e}")
+                logger.error(f"while processing statistics: {stats_data}")
+            else:
                 period_stats = {}
                 for stat_group in groups:
-                    # group of statistics (possession, shots....etc)
+                    # group of statistics (possession, shots...)
                     group_stats = {}
                     for stat in stat_group["statisticsItems"]:
-                        # in a single group there is a divers of statistics
-                        # for instance, shots goup include (shots on target, expected goals....)
-                        stat_name = stat["name"].lower().replace(" ", "_")
-                        group_stats[stat_name] = {
-                            "home": int(stat["homeValue"]),
-                            "away": int(stat["awayValue"])
-                        }
-                        period_stats[stat_group["groupName"].lower()] = group_stats
+                        # in a single group there is divers statistics
+                        # for instance, shots group include (shots on target, expected goals, etc)
+                        try:
+                            stat_name = stat["name"].lower().replace(" ", "_")
+                            group_stats[stat_name] = {
+                                "home": int(stat["homeValue"]),
+                                "away": int(stat["awayValue"])
+                            }
+                            period_stats[stat_group["groupName"].lower()] = group_stats
+                        except KeyError as e:
+                            logger.error(f"KeyError: {e}")
+                            logger.error(f"while processing statistics: {stats_data}")
+                        except Exception as e:
+                            logger.error(f"Unexpected error: {e}")
                 
                 stats[period_data["period"]] = period_stats
-        except KeyError as e:
-            logger.error(f"KeyError: {e}")
-            logger.error(f"while processing statistics: {stats_data}")
-        except Exception as e:
-            logger.error(f"Unexpected error: {e}")
-            logger.error(f"while processing statistics: {stats_data}")
         return stats 
     
     def get_stats(self, match_id):
-        stats_response = self.get_match_data(match_id)
+        stats_response = self.get_match_stats(match_id)
         if stats_response.status_code != 200:
             return None
         stats_json = stats_response.json()["statistics"]
-        extracted_stats = self.extract_stats(stats_json)
+        extracted_stats = self.extract_desired_stats(stats_json)
         return extracted_stats
         
         
@@ -80,6 +87,8 @@ class StatsScraper():
 
 
 if __name__ == "__main__":
+
+    os.environ["PROXIES_PATH"] = r"proxies\valid_proxies.txt"
     stats_scraper = StatsScraper()
-    stats_json = stats_scraper.get_stats(11907976)
-    json_data = json.dump(stats_json, open("stats_data.json", "w"))
+    stats_json = response = json.loads(open(r"dags\test_stats_dag\statistics.json").read())["statistics"]
+    json_data = stats_scraper.extract_stats(stats_json)
